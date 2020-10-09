fd5c81ac8888caf765cc57971ff45d4314f820f1
RobertMcGheecod <rlmiii2004@gmail.com>
2020-10-09 14:54:07 +0000

    I gave values to variables and used them to create the screenshot

--- index.java
+++ index.java
@@ -1,3 +1,11 @@
+int xPos1 = 125;
+int xPos2 = 375;
+
+int yPos1 = 125;
+int yPos2 = 375;
+
+int d = 100;
+
 void setup() {
 	size(500, 500);
 }
@@ -14,12 +22,13 @@ void draw() {
 // change the value of the variables.
 // For example, “variable1 = variable1 + 2;”. 
 // Try different expressions and see what happens!
-
   background(255);
   stroke(0);
-  fill(175);
-
-
+  fill(175, 175, 255);
+  ellipse(xPos1, yPos1, d, d);
+  ellipse(xPos2, yPos1, d, d);
+  ellipse(xPos2, yPos2, d, d);
+  ellipse(xPos1, yPos2, d, d);
   
  
 }
